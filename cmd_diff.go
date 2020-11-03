4683e847a0e4c76b3b27d73fffe6c25023491034
Songmu <y.songmu@gmail.com>
2020-11-03 21:47:47 +0900

    define type runnerImpl and refacter

--- cmd_diff.go
+++ cmd_diff.go
@@ -13,56 +13,50 @@ import (
 	"github.com/sergi/go-diff/diffmatchpatch"
 )
 
-type cmdDiff struct{}
+var cmdDiff = &runnerImpl{
+	name:        "diff",
+	description: "diff of the rule with remote",
+	run: func(ctx context.Context, argv []string, outStream, errStream io.Writer) (err error) {
+		fs := flag.NewFlagSet("ecsched apply", flag.ContinueOnError)
+		fs.SetOutput(errStream)
+		var (
+			conf = fs.String("conf", "", "configuration")
+			rule = fs.String("rule", "", "rule")
+			// all  = fs.Bool("all", false, "apply all rules")
+		)
+		if err := fs.Parse(argv); err != nil {
+			return err
+		}
+		if *rule == "" {
+			return errors.New("-rule option required")
+		}
+		a := getApp(ctx)
+		c := a.Config
+		if *conf != "" {
+			f, err := os.Open(*conf)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			c, err = LoadConfig(f, a.AccountID)
+			if err != nil {
+				return err
+			}
+		}
+		ru := c.GetRuleByName(*rule)
+		if ru == nil {
+			return fmt.Errorf("no rules found for %s", *rule)
+		}
 
-func (cd *cmdDiff) name() string {
-	return "diff"
-}
-
-func (cd *cmdDiff) description() string {
-	return "diff of the rule with remote"
-}
-
-func (cd *cmdDiff) run(ctx context.Context, argv []string, outStream, errStream io.Writer) (err error) {
-	fs := flag.NewFlagSet("ecsched apply", flag.ContinueOnError)
-	fs.SetOutput(errStream)
-	var (
-		conf = fs.String("conf", "", "configuration")
-		rule = fs.String("rule", "", "rule")
-		// all  = fs.Bool("all", false, "apply all rules")
-	)
-	if err := fs.Parse(argv); err != nil {
-		return err
-	}
-	if *rule == "" {
-		return errors.New("-rule option required")
-	}
-	a := getApp(ctx)
-	c := a.Config
-	if *conf != "" {
-		f, err := os.Open(*conf)
+		sess := a.Session
+		svc := cloudwatchevents.New(sess, &aws.Config{Region: &c.Region})
+		from, to, err := ru.diff(ctx, svc)
 		if err != nil {
 			return err
 		}
-		defer f.Close()
-		c, err = LoadConfig(f, a.AccountID)
-		if err != nil {
-			return err
-		}
-	}
-	ru := c.GetRuleByName(*rule)
-	if ru == nil {
-		return fmt.Errorf("no rules found for %s", *rule)
-	}
-
-	sess := a.Session
-	svc := cloudwatchevents.New(sess, &aws.Config{Region: &c.Region})
-	from, to, err := ru.diff(ctx, svc)
-	if err != nil {
+		dmp := diffmatchpatch.New()
+		diffs := dmp.DiffMain(from, to, false)
+		_, err = fmt.Fprint(outStream, dmp.DiffPrettyText(diffs))
 		return err
-	}
-	dmp := diffmatchpatch.New()
-	diffs := dmp.DiffMain(from, to, false)
-	_, err = fmt.Fprint(outStream, dmp.DiffPrettyText(diffs))
-	return err
+	},
 }
---
+++ rule_getter.go
@@ -0,0 +1,99 @@
+package ecsched
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/service/cloudwatchevents"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
+
+type ruleGetter struct {
+	svc                                                                 *cloudwatchevents.CloudWatchEvents
+	clusterArn, ruleArnPrefix, taskDefArnPrefix, roleArnPrefix, roleArn string
+}
+
+func (rg *ruleGetter) getRule(ctx context.Context, r *cloudwatchevents.Rule) (*Rule, error) {
+	if !strings.HasPrefix(*r.Arn, rg.ruleArnPrefix) {
+		return nil, nil
+	}
+
+	ta, err := rg.svc.ListTargetsByRuleWithContext(ctx, &cloudwatchevents.ListTargetsByRuleInput{
+		Rule: r.Name,
+	})
+	if err != nil {
+		return nil, err
+	}
+	var targets []*Target
+	for _, t := range ta.Targets {
+		if *t.Arn != rg.clusterArn {
+			return nil, nil
+		}
+		targetID := *t.Id
+		if targetID == *r.Name {
+			targetID = ""
+		}
+		ecsParams := t.EcsParameters
+		if ecsParams == nil {
+			// ignore rule which have some non ecs targets
+			return nil, nil
+		}
+		target := &Target{TargetID: targetID}
+
+		if role := *t.RoleArn; role != rg.roleArn {
+			target.Role = strings.TrimPrefix(role, rg.roleArnPrefix)
+		}
+
+		taskCount := *ecsParams.TaskCount
+		if taskCount != 1 {
+			target.TaskCount = taskCount
+		}
+		target.TaskDefinition = strings.TrimPrefix(*ecsParams.TaskDefinitionArn, rg.taskDefArnPrefix)
+
+		taskOv := &ecs.TaskOverride{}
+		if err := json.Unmarshal([]byte(*t.Input), taskOv); err != nil {
+			return nil, err
+		}
+		var contOverrides []*ContainerOverride
+		for _, co := range taskOv.ContainerOverrides {
+			var cmd []string
+			for _, c := range co.Command {
+				cmd = append(cmd, *c)
+			}
+			env := map[string]string{}
+			for _, kv := range co.Environment {
+				env[*kv.Name] = *kv.Value
+			}
+			contOverrides = append(contOverrides, &ContainerOverride{
+				Name:        *co.Name,
+				Command:     cmd,
+				Environment: env,
+			})
+		}
+		target.ContainerOverrides = contOverrides
+		targets = append(targets, target)
+	}
+	var desc string
+	if r.Description != nil {
+		desc = *r.Description
+	}
+	ru := &Rule{
+		Name:               *r.Name,
+		Description:        desc,
+		ScheduleExpression: *r.ScheduleExpression,
+		Disabled:           *r.State == "DISABLED",
+	}
+	switch len(targets) {
+	case 0:
+		return nil, nil
+	case 1:
+		ru.Target = targets[0]
+	default:
+		// not supported multiple target yet
+		return nil, nil
+		// ru.Target = targets[0]
+		// ru.Targets = targets[1:]
+	}
+	return ru, nil
+}
